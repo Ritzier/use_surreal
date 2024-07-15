a2e9bcc890d7fda0751ef54d47c6f5a66f101287
ritzier <ritzierwheat@gmail.com>
2024-07-15 12:30:09 +0800

    Add: Github

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod database;
 mod error;
+mod github;
 mod instagram;
 
 use database::Database;
+use github::GitUser;
 use instagram::InstagramUser;
 
 #[tokio::main]
@@ -11,22 +13,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     database.use_ns("test").await?;
     database.use_db("test").await?;
 
-    let user = InstagramUser {
-        username: String::from("Ritzier"),
-        follower: 10,
-        following: 1,
-    };
-
-    let result = database.insert(&user, ("person", &user.username)).await?;
+    let git = GitUser::get_latest("ritzier").await?;
+    let result = database.insert(&git, ("git", "ritzier")).await?;
     dbg!(result);
-    let result = database.insert(&user, ("person", &user.username)).await?;
-    dbg!(result);
-
-    let a: Vec<InstagramUser> = database.get_all("person").await?;
-    dbg!(a);
-
-    let a: Option<InstagramUser> = database.get(("person", "Ritzier")).await?;
-    dbg!(a);
 
     Ok(())
 }
