7111caa13ffa6281379fc54e0c952d718fbb5510
ritzier <ritzierwheat@gmail.com>
2024-07-29 18:07:00 +0800

    .

--- src/instagram.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Serialize, Deserialize, Default)]
-pub struct InstagramUser {
-    pub username: String,
-    pub follower: u32,
-    pub following: u32,
-}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,41 @@
-mod database;
-mod error;
-mod github;
-mod instagram;
+use serde::{Deserialize, Serialize};
+use surrealdb::engine::remote::ws::Ws;
+use surrealdb::opt::auth::Root;
+use surrealdb::sql::Thing;
+use surrealdb::Surreal;
 
-use database::Database;
-use github::GitUser;
-use instagram::InstagramUser;
+#[derive(Debug, Serialize)]
+struct User<'a> {
+    first_name: &'a str,
+    last_name: &'a str,
+    age: u8,
+}
+
+#[derive(Debug, Deserialize)]
+struct Record {
+    #[allow(dead_code)]
+    id: Thing,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let database = Database::init().await?;
-    database.use_ns("test").await?;
-    database.use_db("test").await?;
+    let db = Surreal::new::<Ws>("0.0.0.0:8000").await?;
+    db.signin(Root {
+        username: "root",
+        password: "root",
+    })
+    .await?;
 
-    let git = GitUser::get_latest("ritzier").await?;
-    let result = database.insert(&git, ("git", "ritzier")).await?;
-    dbg!(result);
+    db.use_ns("test").use_db("test").await?;
+
+    let person1 = User {
+        first_name: "ritzier",
+        last_name: "wheat",
+        age: 255,
+    };
+
+    let a: Option<Record> = db.create(("user", "ritzier")).content(person1).await?;
+    dbg!(a);
 
     Ok(())
 }
