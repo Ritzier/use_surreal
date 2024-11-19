19b3000e21438fed53d0981dee0f16c9d5d10b14
ritzier <ritzierwheat@gmail.com>
2024-11-19 20:57:21 +0800

    feat: modify rust code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
+use dotenvy::dotenv;
 use serde::{Deserialize, Serialize};
+use std::env;
 use surrealdb::engine::remote::ws::Ws;
 use surrealdb::opt::auth::Root;
 use surrealdb::sql::Thing;
 use surrealdb::Surreal;
+use thiserror::Error;
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Error)]
+enum Error {
+    #[error("Surrealdb: {0}")]
+    Surreal(#[from] surrealdb::Error),
+    #[error("Dotenv: {0}")]
+    Dotenv(#[from] dotenvy::Error),
+    #[error("Var: {0}")]
+    Var(#[from] std::env::VarError),
+}
+
+type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Debug, Deserialize, Serialize)]
 struct User<'a> {
     first_name: &'a str,
     last_name: &'a str,
@@ -17,12 +32,39 @@ struct Record {
     id: Thing,
 }
 
+struct Config {
+    ip_addr: String,
+    port: String,
+    username: String,
+    password: String,
+}
+
+impl Config {
+    fn load() -> Result<Self> {
+        dotenv()?;
+
+        let ip_addr = env::var("DB_IP")?;
+        let port = env::var("DB_PORT")?;
+        let username = env::var("DB_USER")?;
+        let password = env::var("DB_PASS")?;
+
+        Ok(Self {
+            ip_addr,
+            port,
+            username,
+            password,
+        })
+    }
+}
+
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let db = Surreal::new::<Ws>("0.0.0.0:8000").await?;
+async fn main() -> Result<()> {
+    let config = Config::load()?;
+
+    let db = Surreal::new::<Ws>(&format!("{}:{}", &config.ip_addr, &config.port)).await?;
     db.signin(Root {
-        username: "root",
-        password: "root",
+        username: &config.username,
+        password: &config.password,
     })
     .await?;
 
